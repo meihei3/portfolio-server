4fb598f5455a56ea036943f985a076d5f101377d
meihei3 <24579635+meihei3@users.noreply.github.com>
2022-08-01 00:03:04 +0900

    use X-Real-IP

--- lib/Middleware.go
+++ lib/Middleware.go
@@ -8,7 +8,7 @@ import (
 
 func WithLoggerMiddleware(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("time:%s remote_ip:%s host:%s method:%s uri:%s", time.Now().Format(time.RFC3339), r.RemoteAddr, r.Host, r.Method, r.URL)
+		log.Printf("time:%s remote_ip:%s host:%s method:%s uri:%s", time.Now().Format(time.RFC3339), r.Header.Get("X-Real-IP"), r.Host, r.Method, r.URL)
 		f(w, r)
 	}
 }
